2e12c02b69145cbc3f7e5dd6a0b7d441a2e29230
nicolasjoya <nicolas_joya@javeriana.edu.co>
2024-05-25 12:54:53 -0500

    primer portafolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, HeaderComponent, ProyectsComponent, 
+            SkillsComponent, ContactComponent, AboutComponent, ToastModule,
+            ButtonModule, RippleModule],
+            providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyectoAngular';
+  constructor(private messageService: MessageService) {}
+
+    show() {
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
+    }
 }
